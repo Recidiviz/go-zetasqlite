5e13a45c9b0566c266c1781871d71f01026a9658
Dan Hansen <dan@recidiviz.org>
2025-11-12 02:18:35 -0800

    use typing so all fields are present in struct

--- internal/function.go
+++ internal/function.go
@@ -158,6 +158,12 @@ func STRUCT_FIELD(v Value, idx int) (Value, error) {
 	if err != nil {
 		return nil, err
 	}
+	if sv == nil {
+		return nil, nil
+	}
+	if idx < 0 || idx >= len(sv.values) {
+		return nil, fmt.Errorf("struct field index %d out of range (struct has %d fields)", idx, len(sv.values))
+	}
 	return sv.values[idx], nil
 }
 
